ce55bdf5c8c4d217c9b40a99b0603503cfc0eba0
Sonika <ug201210035@iitj.ac.in>
2014-11-03 08:11:11 +0530

    First edition

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ allRequestUp = []
 allRequestDown = []
 allElevators = []
 allFloorDoors = []
+allRequestEle = []
+allElevatorButtons = []
+# allRequestEle = [[]]
+allRequestEle1 = []
+allRequestEle2 = []
+allRequestEle3 = []
+allRequestEle4 = []
+# allRequestEle.append(allRequestEle4)
+# allRequestEle.append(allRequestEle3)
+# allRequestEle.append(allRequestEle2)
+# allRequestEle.append(allRequestEle1)
 Lift1 = 1
 Lift2 = 2
 Lift3 = 3
@@ -62,6 +73,9 @@ class Example(Frame):
 		#elevators
 		for i in range(4):
 			allElevators.append(Elevator(self.canvas1,i+1,1))
+
+		for i in range(4):
+			allElevatorButtons.append(ElevatorButtons(self.canvas1,i+1))
 
 		self.canvas1.pack()
 
@@ -158,6 +172,47 @@ class Example(Frame):
 			floordoor.update() 
 
 		self.parent.after(5,self.simulate)
+
+class ElevatorButtons(object):
+	def __init__(self,w,elevatorNo):
+		self.elevatorNo = elevatorNo
+		self.window = w
+		self.makeButtons()
+
+	def makeButtons(self):
+		self.Button1 = Button(self.window, text="1", command=lambda: self.createRequestEle(1))
+		self.Button1.place(x=-210+(260*self.elevatorNo),y=5)
+
+		self.Button2 = Button(self.window, text="2", command=lambda: self.createRequestEle(2))
+		self.Button2.place(x=-170+(260*self.elevatorNo),y=5)
+
+		self.Button3 = Button(self.window, text="3", command=lambda: self.createRequestEle(3))
+		self.Button3.place(x=-130+(260*self.elevatorNo),y=5)
+
+		self.Button4 = Button(self.window, text="4", command=lambda: self.createRequestEle(4))
+		self.Button4.place(x=-90+(260*self.elevatorNo),y=5)
+
+		self.Button5 = Button(self.window, text="5", command=lambda: self.createRequestEle(5))
+		self.Button5.place(x=-50+(260*self.elevatorNo),y=5)
+
+		self.Button6 = Button(self.window, text="6", command=lambda: self.createRequestEle(6))
+		self.Button6.place(x=-130+(260*self.elevatorNo),y=40)
+
+		self.Button7 = Button(self.window, text="7", command=lambda: self.createRequestEle(7))
+		self.Button7.place(x=-90+(260*self.elevatorNo),y=40)
+
+		self.stopButton = Button(self.window, text="||", command=lambda: self.createRequestEle(0))
+		self.stopButton.place(x=-50+(260*self.elevatorNo),y=40)
+
+	def createRequestEle(self,requestedFloor):
+		if self.elevatorNo == 1:
+			allRequestEle1.append(requestedFloor)
+		elif self.elevatorNo == 2:
+			allRequestEle2.append(requestedFloor)
+		elif self.elevatorNo == 3:
+			allRequestEle3.append(requestedFloor)
+		else:
+			allRequestEle4.append(requestedFloor)
 
 class RequestButton(object):
 	def __init__(self, w, floorNo):
